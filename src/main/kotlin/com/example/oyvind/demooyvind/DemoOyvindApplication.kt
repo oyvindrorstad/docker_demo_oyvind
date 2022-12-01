7afc94c413c747906d0da815526f5e7ef99d1bad
Øyvind Rørstad <oyvindrorstad@gmail.com>
2022-12-01 15:22:01 +0100

    Init commit

---
+++ src/main/kotlin/com/example/oyvind/demooyvind/DemoOyvindApplication.kt
@@ -0,0 +1,11 @@
+package com.example.oyvind.demooyvind
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoOyvindApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoOyvindApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/oyvind/demooyvind/DemoOyvindApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.oyvind.demooyvind
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoOyvindApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
